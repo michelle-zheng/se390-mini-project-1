5fd0cf42d3fbf77dfdb294a840581444d38ac7cf
RileyHYZ <ri.hyzhou@gmail.com>
2020-09-28 02:28:29 -0700

    added direction delivery

--- server/src/main/java/com/example/server/Controller/SmsController.java
+++ server/src/main/java/com/example/server/Controller/SmsController.java
@@ -1,6 +1,7 @@
 package com.example.server.Controller;
 
 import com.example.server.Model.Sms;
+import com.google.maps.errors.ApiException;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -13,15 +14,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RestController
 public class SmsController {
     private static Logger logger = Logger.getLogger(SmsController.class.getName());
-
-    @Value("${twilio_phone_number}")
-    private String fromNumber;
 
     @Autowired
     public SmsController(
@@ -30,24 +30,36 @@ public class SmsController {
             Twilio.init(ACCOUNT_ID, AUTH_TOKEN);
         }
 
-    @PostMapping("/send")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public void sendMessage(@RequestBody Sms smsDetails) {
-        Message message = Message.creator(new PhoneNumber(smsDetails.getNumber()),
-                new PhoneNumber(fromNumber),smsDetails.getMessage()).create();
-        logger.log(Level.INFO, "Sent message w/ sid: " + message.getSid());
-    }
+//    @PostMapping("/send")
+//    @ResponseStatus(HttpStatus.ACCEPTED)
+//    public void sendMessage(@RequestBody Sms smsDetails) {
+//        Message message = Message.creator(new PhoneNumber(smsDetails.getNumber()), // to
+//                new PhoneNumber(fromNumber),smsDetails.getMessage()).create();     // from
+//        logger.log(Level.INFO, "Sent message w/ sid: " + message.getSid());
+//    }
 
     @PostMapping("/receive")
     @ResponseBody
-    public String receiveMessage(HttpServletRequest request) {
+    public String receiveMessage(HttpServletRequest request) throws InterruptedException, ApiException, IOException {
         String msg = request.getParameter("Body");
         String response;
         if (msg.contains("Hi")) {
             response = "Hello. Please message back your current location and the destination. " +
                     "i.e. from [location] to [location]";
         } else if (msg.contains("from") && msg.contains("to")) { // parse locations
-            response = "Processing your route";
+            int index1 = msg.indexOf("from") + 5;
+            int index2 = msg.indexOf(" to ") + 4;
+            String origin = msg.substring(index1, index2);
+            String destination = msg.substring(index2);
+            GoogleMapAPI googleMapAPI = new GoogleMapAPI();
+            ArrayList<String> directions = googleMapAPI.getDirections(origin, destination);
+
+            Sms sms = new Sms("+16478775992");
+            for (int i = 0; i < directions.size() - 1; ++ i) {
+                sms.setMessage(directions.get(i));
+                sms.sendMessage();
+            }
+            return directions.get(directions.size() - 1);
         } else {
             response = "Failed to read your location request";
         }
--- server/src/main/java/com/example/server/Model/Sms.java
+++ server/src/main/java/com/example/server/Model/Sms.java
@@ -1,9 +1,32 @@
 package com.example.server.Model;
 
 
+import com.example.server.Config;
+import com.example.server.Controller.GoogleMapAPI;
+import com.example.server.Controller.SmsController;
+import com.google.maps.errors.ApiException;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Sms {
+    private static Logger logger = Logger.getLogger(Sms.class.getName());
+    static private Config instance = Config.getInstance();
+
     private String number;
     private String message;
+
+//    @Value("${twilio_phone_number}")
+//    private String fromNumber;
+
+    public Sms(String number) {
+        this.number = number;
+    }
 
     public Sms(String number, String message) {
         this.number = number;
@@ -18,4 +41,9 @@ public class Sms {
 
     public void setMessage(String message) { this.message = message; }
 
+    public void sendMessage() {
+        Message msg = Message.creator(new PhoneNumber(number), // to
+                new PhoneNumber(instance.getProperty("twilio_phone_number")), message).create();     // from
+        logger.log(Level.INFO, "Sent message w/ sid: " + msg.getSid());
+    }
 }
