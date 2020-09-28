d76d510ca89fd277f22b83d1ea81f227c1441526
Michelle <zheng.michelle.99@gmail.com>
2020-09-28 14:19:10 -0400

    Send SMS with SMS Manager

--- Android/app/src/main/java/com/example/msgnav/SearchActivity.kt
+++ Android/app/src/main/java/com/example/msgnav/SearchActivity.kt
@@ -1,11 +1,8 @@
 package com.example.msgnav
 
-import android.R.attr.phoneNumber
-import android.R.id.message
 import android.app.Activity
 import android.app.PendingIntent
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.telephony.SmsManager
 import android.view.View
@@ -47,10 +44,9 @@ class SearchActivity : Activity(), SearchRecyclerViewAdapter.OnDataChangedListen
 
     fun onDoneButtonClick(view: View) {
         // TODO: Send the SMS Message
+
         val locations: Array<String> = viewAdapter.getItems()
-        val intent = Intent(this, SendSms::class.java)
-        intent.putExtra("locations", locations)
-        startActivityForResult(intent, 1)
+        sendSms(locations)
 
 //        val intent = Intent(this, FullDirectionsActivity::class.java)
 //
@@ -84,33 +80,13 @@ class SearchActivity : Activity(), SearchRecyclerViewAdapter.OnDataChangedListen
 
     private fun sendSms(data: Array<String>) {
         try {
-//            val smgr = SmsManager.getDefault() as SmsManager
-//            val sentPI: PendingIntent
-//            val SENT = "SMS_SENT"
-//
-//            sentPI = PendingIntent.getBroadcast(this, 0, Intent(SENT), 0)
-//
-//            smgr.sendTextMessage(
-//                "+16474786822",
-//                null,
-//                "from " + data[0] + " to " + data[1],
-//                sentPI,
-//                null
-//            )
-//            smgr.sendTextMessage(
-//                "+13852090925",
-//                null,
-//                "from " + data[0] + " to " + data[1],
-//                null,
-//                null
-//            )
-            val uri = Uri.parse("smsto:+13852090925")
-            val intent = Intent(Intent.ACTION_SENDTO, uri)
-            intent.putExtra("sms_body", "from " + data[0] + " to " + data[1])
-            intent.putExtra("exit_on_sent", true);
-//            startActivity(intent)
-            startActivityForResult(intent, 1);
-            Toast.makeText(this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show()
+            SmsManager.getDefault().sendTextMessage(
+                "+13852090925",
+                null,
+                "from " + data[0] + " to " + data[1],
+                null,
+                null
+            )
         } catch (e: Exception) {
             Toast.makeText(
                 this,
