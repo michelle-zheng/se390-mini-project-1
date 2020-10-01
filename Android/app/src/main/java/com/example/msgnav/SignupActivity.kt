8a2095122cf2b46ca7c5f47ed6eb660b64c064d4
LeoLuheng <luheng.leo@gmail.com>
2020-10-01 15:20:38 -0700

    WIP: integrate registration api call

--- Android/app/src/main/java/com/example/msgnav/SignupActivity.kt
+++ Android/app/src/main/java/com/example/msgnav/SignupActivity.kt
@@ -5,16 +5,35 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.core.content.edit
+import okhttp3.*
+import java.io.IOException
 
 class SignupActivity: Activity() {
+    // api call client
+    private val client = OkHttpClient();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
     }
 
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body?.string())
+        })
+    }
+
     fun onClickRegisterButton(view: View) {
         // TODO: HIT THE API TO REGISTER
+        val phoneNumber: TextView = findViewById(R.id.pnumber);
+        phoneNumber.text;
 
         getSharedPreferences("shared_prefs", Context.MODE_PRIVATE).edit {
             putBoolean("is_registered", true)
